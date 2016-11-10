917608958aa14256446bce1c3208d2fb62ffe820
SayakaNagai <1355059t@stu.kobe-u.ac.jp>
2016-11-10 13:21:28 +0900

    161110

--- App/IPLab_key/run.sh
+++ App/IPLab_key/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-./IPLab_key /Users/sayaka/Documents/data/wt_N2_030131_01.bdml0.18.xml /Users/sayaka/Documents/data/wt_N2_030131_01
+PROGRAM=${PWD##*/}
+./$PROGRAM ~/Work/Data/C.elegans/wt_N2_030131_01.bdml0.18.xml ~/Work/Data/C.elegans/wt_N2_030131_01
